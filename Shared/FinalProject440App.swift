8a5b9b8043379b9e62aa3cac9e807b869fa87063
Katia <katiafloresbasterrechea@gmail.com>
2022-05-04 21:21:23 -0500

    Initial Commit

---
+++ Shared/FinalProject440App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject440App.swift
+//  Shared
+//
+//  Created by Katia Flores on 4/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject440App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
